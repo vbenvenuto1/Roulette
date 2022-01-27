9458ea941bcb8344554733e87aa0b824025f8c7c
tamim08cse <tamimhasan08@gmail.com>
2022-01-28 00:13:51 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+console.log("hello from main.js")
+
+// Add eventlisteners to each table cell
+
+// When the user clicks the cell, "place" their bet in that cell
+
+// if the user clicks the spin btn identify bets on the table
+
+// Compare bets with winning cell
+
+// Print output of the game
+
+// If the user clicks the clear btn remove bets from the table (great for mistakes)
+
+// To play, randomly pick from the gameboard for the winning location
+
+// Keep track of users money
+
+// If user has no money then obviosly they can't play
+
+// Add eventlisteners to each table cell
+
+// When the user clicks the cell, "place" their bet in that cell
+
+// if the user clicks the spin btn identify bets on the table
+
+// Compare bets with winning cell
+
+// Print output of the game
+
+// If the user clicks the clear btn remove bets from the table (great for mistakes)
+
+// To play, randomly pick from the gameboard for the winning location
+
+// Keep track of users money
+
+// If user has no money then obviosly they can't play
